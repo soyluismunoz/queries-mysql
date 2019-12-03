b0bc773b778ed07f31219de1176cc43607ae6d18
Luis Muñoz <luis152288@gmail.com>
2019-12-03 16:58:19 -0400

    Update trigger-slug.sql

--- trigger-slug.sql
+++ trigger-slug.sql
@@ -1,51 +1,38 @@
-CREATE TRIGGER `slugify` BEFORE INSERT ON `nombres` FOR EACH ROW
+CREATE TRIGGER `slugify` BEFORE INSERT ON `products` FOR EACH ROW
 BEGIN
 	DECLARE
 		new_slug VARCHAR ( 255 );
 	DECLARE
 		slug_counter INT;
-	
 	SET slug_counter = 1;
 	
-	SET new_slug = new.title;
-	
+	SET new_slug = new.name;
 	SET new_slug = LOWER( TRIM( new_slug ) );
-	
 	SET new_slug = REPLACE ( new_slug, ':', '' );
-	
 	SET new_slug = REPLACE ( new_slug, ')', '' );
-	
 	SET new_slug = REPLACE ( new_slug, '(', '' );
-	
 	SET new_slug = REPLACE ( new_slug, ',', '' );
-	
 	SET new_slug = REPLACE ( new_slug, '.', '' );
-	
 	SET new_slug = REPLACE ( new_slug, "?", '' );
-	
 	SET new_slug = REPLACE ( new_slug, ';', '' );
-	
 	SET new_slug = REPLACE ( new_slug, '&', '' );
-	
 	SET new_slug = REPLACE ( new_slug, '!', '' );
-	
 	SET new_slug = REPLACE ( new_slug, '.', '' );
-	
 	SET new_slug = REPLACE ( new_slug, '€', '' );
-	
 	SET new_slug = REPLACE ( new_slug, 'euro;', '' );
-	
 	SET new_slug = REPLACE ( new_slug, ' ', '-' );
-	
 	SET new_slug = REPLACE ( new_slug, '--', '-' );
-	
 	SET new_slug = REPLACE ( new_slug, '--', '-' );
-	
 	SET new_slug = REPLACE ( new_slug, '--', '-' );
-	
+  SET new_slug = REPLACE ( new_slug, '"', '-' );
+  SET new_slug = REPLACE ( new_slug, '%', '' );
+  SET new_slug = REPLACE ( new_slug, 'º', '' );
+  SET new_slug = REPLACE ( new_slug, 'ª', '' );
+  SET new_slug = REPLACE ( new_slug, '/', '-' );
+  SET new_slug = REPLACE ( new_slug, '#', '' );
 	SET new.slug = new_slug;
 	WHILE
-			EXISTS ( SELECT TRUE FROM test WHERE slug = new.slug ) DO
+			EXISTS ( SELECT TRUE FROM products WHERE slug = new.slug ) DO
 			
 			SET new.slug = concat( new_slug, '-', slug_counter );
 		
