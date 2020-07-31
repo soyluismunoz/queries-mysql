00cb6c16527108b2274bcd94b574fc791074d6bc
Luis Muñoz <luis152288@gmail.com>
2020-07-31 15:51:49 -0400

    tablas relacionadas

--- generales.sql
+++ generales.sql
@@ -6,3 +6,7 @@ UPDATE products SET slug=concat(left(slug,length(slug) -1),'-')
 
 //opcional
 WHERE RIGHT(slug, 1) = '-'
+
+
+//tablas realcionadas 
+SELECT `bookingStart`,`bookingEnd`, `customFields`, `info` FROM `yah_amelia_appointments` INNER JOIN `yah_amelia_customer_bookings` ON `yah_amelia_appointments`.`ID` = `yah_amelia_customer_bookings`.`appointmentId` WHERE `bookingStart` LIKE "%2020-07-13 07:00:00%" 
