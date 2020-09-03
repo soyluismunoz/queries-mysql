c6622954c8dca3384deef09f1230dfe34f626bd2
Luis Muñoz <luis152288@gmail.com>
2020-09-03 10:41:32 -0400

    Update generales.sql

--- generales.sql
+++ generales.sql
@@ -10,3 +10,17 @@ WHERE RIGHT(slug, 1) = '-'
 
 //tablas realcionadas 
 SELECT `bookingStart`,`bookingEnd`, `customFields`, `info` FROM `yah_amelia_appointments` INNER JOIN `yah_amelia_customer_bookings` ON `yah_amelia_appointments`.`ID` = `yah_amelia_customer_bookings`.`appointmentId` WHERE `bookingStart` LIKE "%2020-07-13 07:00:00%" 
+
+//borrar post viejos en wordpress
+UPDATE `wp_posts` SET `post_status` = REPLACE(`post_status`,'publish','trash')WHERE `post_date` < '2016-10-12 00:00:00' AND `post_type` = 'post' AND `post_status` = 'publish' 
+
+// traer los post de una determinada categoria wordpress
+SELECT * FROM wp_posts
+    LEFT JOIN wp_term_relationships ON
+    wp_posts.ID = wp_term_relationships.object_id
+    LEFT JOIN wp_term_taxonomy ON
+    wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+    WHERE wp_posts.post_status = "publish"
+    AND wp_term_taxonomy.taxonomy = "category"
+    AND wp_term_taxonomy.term_id = 11
+    ORDER BY post_date DESC
