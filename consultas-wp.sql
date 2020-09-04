aa2f8b6d687b03187ff596857d2e2274f36b2971
Luis Muñoz <luis152288@gmail.com>
2020-09-04 17:20:12 -0400

    Create consultas-wp.sql

---
+++ consultas-wp.sql
@@ -0,0 +1,13 @@
+//borrar post viejos en wordpress
+UPDATE `wp_posts` SET `post_status` = REPLACE(`post_status`,'publish','trash')WHERE `post_date` < '2016-10-12 00:00:00' AND `post_type` = 'post' AND `post_status` = 'publish' 
+
+// traer los post de una determinada categoria wordpress
+SELECT * FROM wp_posts
+    LEFT JOIN wp_term_relationships ON
+    wp_posts.ID = wp_term_relationships.object_id
+    LEFT JOIN wp_term_taxonomy ON
+    wp_term_relationships.term_taxonomy_id = wp_term_taxonomy.term_taxonomy_id
+    WHERE wp_posts.post_status = "publish"
+    AND wp_term_taxonomy.taxonomy = "category"
+    AND wp_term_taxonomy.term_id = 11
+    ORDER BY post_date DESC
