7413037647f1411b76d6736a3476914466eb2a75
Luis Muñoz <luis152288@gmail.com>
2020-12-20 22:54:07 -0400

    Update registros-duplicados.sql

--- registros-duplicados.sql
+++ registros-duplicados.sql
@@ -11,3 +11,18 @@ HAVING
 	Count( * ) > 1;
 	
 	
+SELECT 
+	`cashier_name`,
+	`trasaction_type`,
+	Count(*) AS `Cantidad`,
+	SUM(`input`) as total
+    
+FROM 
+	transactiondata 
+WHERE 
+ `cashier_id` = 4
+GROUP BY
+	`cashier_name`,
+	`trasaction_type` 
+HAVING
+	Count( * ) > 0;
