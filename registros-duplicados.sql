5431af4921893d47da58eae5b1637700bc4ac17e
Luis Muñoz <luis152288@gmail.com>
2020-12-20 23:13:08 -0400

    reporte de ingreso y egreso

--- registros-duplicados.sql
+++ registros-duplicados.sql
@@ -11,16 +11,17 @@ HAVING
 	Count( * ) > 1;
 	
 	
+	
 SELECT 
 	`cashier_name`,
 	`trasaction_type`,
 	Count(*) AS `Cantidad`,
-	SUM(`input`) as total
-    
+	if(`operation` = 2, SUM(`input`), 0) as ingreso,
+	if(`operation` = 1, SUM(`output`), 0) as egreso
 FROM 
 	transactiondata 
 WHERE 
- `cashier_id` = 4
+ 	`cashier_id` = 4
 GROUP BY
 	`cashier_name`,
 	`trasaction_type` 
