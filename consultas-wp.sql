d0d3f308840ec19c005746ebba7c81d79d01e9c2
Luis Muñoz <luis152288@gmail.com>
2023-09-13 10:48:44 -0400

    Update consultas-wp.sql

--- consultas-wp.sql
+++ consultas-wp.sql
@@ -1,7 +1,7 @@
-//borrar post viejos en wordpress
+/*borrar post viejos en wordpress*/
 UPDATE `wp_posts` SET `post_status` = REPLACE(`post_status`,'publish','trash')WHERE `post_date` < '2016-10-12 00:00:00' AND `post_type` = 'post' AND `post_status` = 'publish' 
 
-// traer los post de una determinada categoria wordpress
+/* traer los post de una determinada categoria wordpress*/
 SELECT * FROM wp_posts
     LEFT JOIN wp_term_relationships ON
     wp_posts.ID = wp_term_relationships.object_id
@@ -12,7 +12,7 @@ SELECT * FROM wp_posts
     AND wp_term_taxonomy.term_id = 11
     ORDER BY post_date DESC
 
-//cambiar urls
+/*cambiar urls*/
 UPDATE wp_options SET option_value = REPLACE ( option_value, 'dominioA.com', 'dominioB.com' );
 UPDATE wp_posts SET guid = REPLACE ( guid, 'dominioA.com', 'dominioB.com' );
 UPDATE wp_posts SET post_content = REPLACE ( post_content, 'dominioA.com', 'dominioB.com' );
